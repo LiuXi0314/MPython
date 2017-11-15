020a451fc3db70628e9150049e4096de4f4035ac
LiuXi0314 <xiaoxixizhizhi@gmail.com>
2017-11-15 18:24:54 +0800

    添加 fun 模块

---
+++ function/BaseFun.py
@@ -0,0 +1,17 @@
+print(abs(1))
+print(abs(-2))
+print(abs(3.33))
+print(abs(False))
+# print(abs('str')) error
+print(max(1, 2, 3, -2, True))
+
+print(bool("23"))
+print(int("23"))
+
+# 定义函数
+
+def myFirstFun():
+    print("Hello %s" % 'My First Function')
+
+
+myFirstFun()
